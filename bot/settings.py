582ab9d558aa9b0425a135783f33399657d8e44b
KadilenkoIvan <kadilenkoivan@gmail.com>
2025-09-18 15:17:57 +0500

    деление на директории

--- bot/settings.py
+++ bot/settings.py
@@ -6,9 +6,11 @@ load_dotenv()
 
 SERVICE_ACCOUNT_ID = os.getenv("SERVICE_ACCOUNT_ID")
 KEY_ID = os.getenv("KEY_ID")
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 FOLDER_ID = os.getenv("FOLDER_ID")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+with open("private_key.pem", "r") as f:
+    PRIVATE_KEY = f.read()
 
 S3_ENDPOINT = os.getenv("S3_ENDPOINT")
 S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
