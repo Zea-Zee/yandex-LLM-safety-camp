47096b349d9b8bceefb2c3491259bfc4958b7d80
Gleb <glebru.irk38@yandex.ru>
2025-09-23 20:38:28 +0500

    run docker

--- run_docker.sh
+++ run_docker.sh
@@ -5,42 +5,40 @@ set -e
 # 1. Создаем Docker сеть
 # -------------------------
 echo "Creating network..."
-sudo docker network create --driver bridge microservices-network 2>/dev/null || true
+docker network create --driver bridge microservices-network 2>/dev/null || true
 
 # -------------------------
 # 2. Собираем базовый образ
 # -------------------------
 echo "Building base image..."
-sudo docker build -f base.Dockerfile -t base:3.11 .
+docker build -f base.Dockerfile -t base:3.11 .
 
 # -------------------------
 # 3. Собираем микросервисы
 # -------------------------
-echo "Building logger..."
-sudo docker build -t logger-image ./logger
-
 echo "Building bot..."
-sudo docker build -t bot-image ./bot
+docker build -t bot-image ./bot
 
 echo "Building moderator..."
-sudo docker build -t moderator-image ./moderator
+docker build -t moderator-image ./moderator
 
 echo "Building orchestrator..."
-sudo docker build -t orchestrator-image ./orchestrator
+docker build -t orchestrator-image ./orchestrator
 
 echo "Building rag..."
-sudo docker build -t rag-image ./rag
+docker build -t rag-image ./rag
 
 echo "Building yandex_gpt..."
-sudo docker build -t yandex_gpt-image ./yandex_gpt
+docker build -t yandex_gpt-image ./yandex_gpt
+
+echo "Building logger..."
+docker build -t logger-image ./logger
 
 # -------------------------
 # 4. Останавливаем старые контейнеры (если есть)
 # -------------------------
 echo "Stopping old containers..."
-
-#sudo docker rm -f bot moderator orchestrator rag yandex_gpt 2>/dev/null || true
-sudo docker rm -f bot moderator orchestrator rag yandex_gpt logger
+docker rm -f bot moderator orchestrator rag yandex_gpt 2>/dev/null || true
 
 # -------------------------
 # 5. Запуск контейнеров в общей сети
@@ -48,20 +46,38 @@ sudo docker rm -f bot moderator orchestrator rag yandex_gpt logger
 echo "Running containers..."
 
 # Запускаем сервисы в правильном порядке (сначала зависимости)
-
-sudo docker run -d --name logger --network microservices-network -p 8020:8020 logger-image
-sudo docker run -d --name orchestrator --network microservices-network -p 8003:8003 --env-file ./orchestrator/.env orchestrator-image
-sudo docker run -d --name yandex_gpt --network microservices-network -p 8000:8000 --env-file ./yandex_gpt/.env yandex_gpt-image
-sudo docker run -d --name rag --network microservices-network -p 8002:8002 --env-file ./rag/.env rag-image
-sudo docker run -d --name moderator --network microservices-network -p 8001:8001 --env-file ./moderator/.env moderator-image
-sudo docker run -d --name bot --network microservices-network --env-file ./bot/.env bot-image
+docker run -d --name yandex_gpt --network microservices-network -p 8000:8000 yandex_gpt-image
+docker run -d --name rag --network microservices-network -p 8002:8002 rag-image
+docker run -d --name moderator --network microservices-network -p 8001:8001 moderator-image
+docker run -d --name orchestrator --network microservices-network -p 8003:8003 orchestrator-image
+docker run -d --name bot --network microservices-network bot-image
+docker run -d --name logger --network microservices-network logger-image
 
 echo "All services are running. Final check:"
 sleep 5
-sudo docker ps
+docker ps
 
 # -------------------------
 # 6. Проверка сети
 # -------------------------
 echo "Network info:"
-sudo docker network inspect microservices-network
+docker network inspect microservices-network
+
+
+# Тегируем образы
+docker tag orchestrator-image:latest cr.yandex/crphfppr95vssc70laib/orchestrator-image:latest
+docker tag moderator-image:latest cr.yandex/crphfppr95vssc70laib/moderator-image:latest
+docker tag bot-image:latest cr.yandex/crphfppr95vssc70laib/bot-image:latest
+docker tag yandex_gpt-image:latest cr.yandex/crphfppr95vssc70laib/yandex_gpt-image:latest
+docker tag rag-image:latest cr.yandex/crphfppr95vssc70laib/rag-image:latest
+docker tag logger-image:latest cr.yandex/crphfppr95vssc70laib/logger-image:latest
+docker tag base:3.11 cr.yandex/crphfppr95vssc70laib/base:3.11
+
+# Пушим образы
+docker push cr.yandex/crphfppr95vssc70laib/orchestrator-image:latest
+docker push cr.yandex/crphfppr95vssc70laib/moderator-image:latest
+docker push cr.yandex/crphfppr95vssc70laib/bot-image:latest
+docker push cr.yandex/crphfppr95vssc70laib/yandex_gpt-image:latest
+docker push cr.yandex/crphfppr95vssc70laib/rag-image:latest
+docker push cr.yandex/crphfppr95vssc70laib/logger-image:latest
+docker push cr.yandex/crphfppr95vssc70laib/base:3.11
